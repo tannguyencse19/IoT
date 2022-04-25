102d845045371634bac9c5748204282999ae78eb
Tan Nguyen <tannguyen.cse19@gmail.com>
2022-04-25 17:12:05 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,57 @@
-print("Xin chào ThingsBoard")
-import paho.mqtt.client as mqttclient
-import time
 import json
+import time
+import paho.mqtt.client as mqttclient
+import serial
+
+print("IoT Gateway")
 
 BROKER_ADDRESS = "demo.thingsboard.io"
 PORT = 1883
+
+mess = ""
+
 THINGS_BOARD_ACCESS_TOKEN = "VvYKouCOZy2aIV0yHfWh"
+bbc_port = "COM8"
+if len(bbc_port) > 0:
+    ser = serial.Serial(port=bbc_port, baudrate=115200)
+
+
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    # TODO: Add your source code to publish data to the server
+    collect_data = {}
+    if (splitData[1] == "TEMP"):
+        collect_data["temperature"] = splitData[2]
+    if (splitData[1] == "LIGHT"):
+        collect_data["light"] = splitData[2]
+    client.publish("v1/devices/me/telemetry", json.dumps(collect_data), 1)
+
+# collect_data = {
+#         'temperature': temp,
+#         'humidity': humi,
+#         'light': light_intesity,
+#         # 'longitude': longitude,
+#         # 'latitude': latitude,
+#     }
+
+
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        print(mess)
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
 
 
 def subscribed(client, userdata, mid, granted_qos):
@@ -15,13 +61,31 @@ def subscribed(client, userdata, mid, granted_qos):
 def recv_message(client, userdata, message):
     print("Received: ", message.payload.decode("utf-8"))
     temp_data = {'value': True}
+    cmd = 69
+    # TODO: Update the cmd to control 2 devices
     try:
         jsonobj = json.loads(message.payload)
-        if jsonobj['method'] == "setValue":
+        if jsonobj['method'] == "setLED":
             temp_data['value'] = jsonobj['params']
-            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+            client.publish('v1/devices/me/BUTTON_LED',
+                           json.dumps(temp_data), 1)
+            if (jsonobj['params'] == True):
+                cmd = 1
+            else:
+                cmd = 0
+        if jsonobj['method'] == "setFAN":
+            temp_data['value'] = jsonobj['params']
+            client.publish('v1/devices/me/BUTTON_FAN',
+                           json.dumps(temp_data), 1)
+            if (jsonobj['params'] == True):
+                cmd = 3
+            else:
+                cmd = 2
     except:
         pass
+
+    if len(bbc_port) > 0:
+        ser.write((str(cmd) + "#").encode())
 
 
 def connected(client, usedata, flags, rc):
@@ -42,24 +106,8 @@ client.loop_start()
 client.on_subscribe = subscribed
 client.on_message = recv_message
 
-temp = 30
-humi = 50
-light_intesity = 100
-counter = 0
-
-latitude = 13.7830
-longitude = 109.2197
-
 while True:
-    collect_data = {
-        'temperature': temp,
-        'humidity': humi,
-        'light': light_intesity,
-        'longitude': longitude,
-        'latitude': latitude,
-    }
-    temp += 1
-    humi += 1
-    light_intesity += 1
-    client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
-    time.sleep(5)
+    if len(bbc_port) > 0:
+        readSerial()
+
+    time.sleep(1)
